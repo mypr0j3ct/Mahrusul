527b4e5d4a10e456f62e854d439eff9a2e08d651
mypr0j3ct <123tychan@gmail.com>
2025-05-10 17:55:46 +0700

    Create config-sdk.js

---
+++ Revisi Eval 1/config-sdk.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+import { getDatabase, ref, set, onValue} from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDX9FQQai2rWeUubCX903z-yPMro_TGTIQ",
+//   authDomain: "projectv1-a9190.firebaseapp.com",
+//   databaseURL: "https://projectv1-a9190-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "projectv1-a9190",
+//   storageBucket: "projectv1-a9190.firebasestorage.app",
+//   messagingSenderId: "343955967254",
+//   appId: "1:343955967254:web:6de9646cb6140a07a89437",
+//   measurementId: "G-N903P4FH5W"
+// };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export {app}
